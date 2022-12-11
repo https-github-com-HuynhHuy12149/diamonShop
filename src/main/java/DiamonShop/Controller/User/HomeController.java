4dea81597786798c88ae07ef272e1b89b6550367
hung1127 <nguyenthanhhung200111@gmail.com.vn>
2022-12-11 20:17:08 +0700

    HomeController

--- src/main/java/DiamonShop/Controller/User/HomeController.java
+++ src/main/java/DiamonShop/Controller/User/HomeController.java
@@ -11,20 +11,20 @@ import DiamonShop.Service.User.HomeImpl;
 @Controller
 public class HomeController extends BaseController {
 	
-//	@RequestMapping(value = {"/","/trang-chu"})
-//	public ModelAndView Index() {
-////		ModelAndView mv = new ModelAndView("user/index");
-//		
-//		_mvShare.addObject("slides",_homeService.GetDataSlides());
-//		_mvShare.addObject("categorys",_homeService.GetDataCategorys());
-//		_mvShare.addObject("products", _homeService.GetDataProducts());
-//		_mvShare.setViewName("user/index");
-//		return _mvShare;
-//	}
-//	
-//	@RequestMapping(value = {"/product"})
-//	public ModelAndView Product() {
-//		ModelAndView mv = new ModelAndView("user/product");
-//		return mv;
-//	}
+	@RequestMapping(value = {"/","/trang-chu"})
+	public ModelAndView Index() {
+		ModelAndView mv = new ModelAndView("user/index");
+		
+		_mvShare.addObject("slides",_homeService.GetDataSlides());
+		_mvShare.addObject("categorys",_homeService.GetDataCategorys());
+		_mvShare.addObject("products", _homeService.GetDataProducts());
+		_mvShare.setViewName("user/index");
+		return _mvShare;
+	}
+	
+	@RequestMapping(value = {"/product"})
+	public ModelAndView Product() {
+		ModelAndView mv = new ModelAndView("user/product");
+		return mv;
+	}
 }
